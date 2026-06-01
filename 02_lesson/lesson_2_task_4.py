eda65fe4d1366f7727925246be027eb09c80d284
ArturSh2403 <sharturchikccc@mail.ru>
2026-06-01 13:53:57 +1000

    Поменял условие в 3 строке

--- 02_lesson/lesson_2_task_4.py
+++ 02_lesson/lesson_2_task_4.py
@@ -1,6 +1,6 @@
 def fizz_buzz(n):
     for num in range(1, n + 1):
-        if num % 3 == 0 and num % 5 == 0:
+        if num % 15 == 0:
             print("FizzBuzz")
         elif num % 3 == 0:
             print("Fizz")
@@ -9,4 +9,4 @@ def fizz_buzz(n):
         else:
             print(num)
 
-fizz_buzz(15) # noqa
+fizz_buzz(10) # noqa
